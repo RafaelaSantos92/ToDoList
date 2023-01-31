d3a6192f710c34917cc19c8979c0f4596058ff54
Rafaela Santos <santos.rafaela2991@gmail.com>
2023-01-31 09:37:32 -0300

    Implementação Get/Post
    Implementação Get/Post

--- src/main/java/com/lasprogramaderas/todolist/controller/TaskController.java
+++ src/main/java/com/lasprogramaderas/todolist/controller/TaskController.java
@@ -1,5 +1,45 @@
 package com.lasprogramaderas.todolist.controller;
 
-public class TaskController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lasprogramaderas.todolist.model.Task;
+import com.lasprogramaderas.todolist.service.TaskService;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TaskController {
+	
+	
+	@Autowired
+	TaskService taskservice;
+	
+	
+	@PostMapping("/tasks")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Task createTask(@RequestBody Task task) {
+		return taskservice.createTask(task);
+	}
+	
+	@GetMapping("/tasks")
+	@ResponseStatus(HttpStatus.OK)
+	public List<Task> getAllTasks(){
+		return taskservice.listAllTasks();
+	}
+	
+	@GetMapping("/tasks/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Task> getTaskById(@PathVariable (value = "id") Long id){
+		return taskservice.findTaskById(id);
+	}
 }
--- src/main/java/com/lasprogramaderas/todolist/service/TaskService.java
+++ src/main/java/com/lasprogramaderas/todolist/service/TaskService.java
@@ -1,5 +1,37 @@
 package com.lasprogramaderas.todolist.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.lasprogramaderas.todolist.model.Task;
+import com.lasprogramaderas.todolist.repository.TaskRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class TaskService {
+	
+	
+	@Autowired
+	private TaskRepository taskrepository;
+	
+	
+	public Task createTask(Task task) {
+		return taskrepository.save(task);
+	}
+	
+	public List<Task> listAllTasks(){
+		return taskrepository.findAll();
+	}
+	
+	public ResponseEntity<Task> findTaskById(Long id){
+		return taskrepository.findById(id)
+				.map(task -> ResponseEntity.ok().body(task))
+				.orElse(ResponseEntity.notFound().build());
+	}
 
 }
